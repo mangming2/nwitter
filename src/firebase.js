67748a263746536aa4bdc3094e3e9e54c4339645
mangming2 <59263564+mangming2@users.noreply.github.com>
2021-09-08 23:18:57 +0900

    9-8 router

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBHc-wIgryenrPu81tudZlpMt3fLQCMN9o",
+    authDomain: "nwitter-ac781.firebaseapp.com",
+    projectId: "nwitter-ac781",
+    storageBucket: "nwitter-ac781.appspot.com",
+    messagingSenderId: "724084165181",
+    appId: "1:724084165181:web:deafe218c1a105ed48b854"
+  };
+
+  export default  firebase.initializeApp(firebaseConfig);
